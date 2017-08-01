7927458e6d3c0bce52fd735fabce0df672490c50
Killian Huang <xhuang1@andrew.cmu.edu>
2017-08-01 17:12:18 -0400

    correct TA data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-courses = Course.create([{ta_name: 'ta1', course_num: '21127', days_of_week: 'MTWTHF', time_of_day: '1030'},
-  {ta_name: 'ta2', course_num: '21259', days_of_week: 'MTWTHF', time_of_day: '0900'},
-  {ta_name: 'ta3', course_num: '70122', days_of_week: 'MTWTHF', time_of_day: '0130'}])
+courses = Course.create([{ta_name: 'TA127', course_num: '21127', days_of_week: 'MTWTHF', time_of_day: '0900'},
+  {ta_name: 'TA259', course_num: '21259', days_of_week: 'MTWTHF', time_of_day: '0900'},
+  {ta_name: 'TA122', course_num: '70122', days_of_week: 'MTWTHF', time_of_day: '1030'},
+  {ta_name: 'TA1030', course_num: '21127', days_of_week: 'TTH', time_of_day: '1030'},
+  {ta_name: 'TA1230', course_num: '18202', days_of_week: 'TTH', time_of_day: '1230'},
+  {ta_name: 'TA1630', course_num: '21256', days_of_week: 'T', time_of_day: '1630'}])
